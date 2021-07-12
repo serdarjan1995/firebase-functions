d3e440e7f44483e6df0a3b16d7e05d26241bf99c
Sardor <serdarjan1995@gmail.com>
2021-07-12 16:38:03 +0300

    restrict uploading: fix axios not used issue

--- src/UploadFile.js
+++ src/UploadFile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Container, withStyles} from '@material-ui/core';
 import {firebaseConfig} from "./firebaseApp";
-import axios from "axios";
+//import axios from "axios";
 
 const styles = theme => ({
     root: {
